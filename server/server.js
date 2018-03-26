4d7a44ebe229d7df22e73e6db718c19caaf55743
Luca Montanari <luca.montanari@libero.it>
2018-03-26 10:39:14 +0200

    mongolab

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/book_db');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/book_db');
 
 const { Book } = require('./models/books');
 const { Store } = require('./models/stores');
